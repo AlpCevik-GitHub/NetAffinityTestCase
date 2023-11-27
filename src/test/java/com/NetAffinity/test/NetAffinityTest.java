7f1b50de96ea7f8293c34be94eb1aa0e0987eb1f
Alp <Alpcevik19@gmail.com>
2023-11-27 15:55:16 +0300

    Third Commit

--- src/test/java/com/NetAffinity/test/NetAffinityTest.java
+++ src/test/java/com/NetAffinity/test/NetAffinityTest.java
@@ -1,9 +1,7 @@
 package com.NetAffinity.test;
 
 import com.NetAffinity.pages.AddPage;
-import com.NetAffinity.pages.BasePage;
 import com.NetAffinity.utilities.BrowserUtils;
-import com.NetAffinity.utilities.ConfigurationReader;
 import com.NetAffinity.utilities.Driver;
 import groovy.util.logging.Slf4j;
 import org.apache.logging.log4j.LogManager;
@@ -12,13 +10,16 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+
 import org.testng.annotations.Test;
-import groovy.util.logging.Slf4j;
+
 
 @Slf4j
 public class NetAffinityTest extends TestBase {
+
+
+     /* in this class, we put test steps, @Test annotation is used to execute method
+     */
 
     AddPage addPage = new AddPage();
     int actualAccountNumber;
@@ -31,26 +32,39 @@ public class NetAffinityTest extends TestBase {
     public void test01() {
 
         System.out.println("totalNumberOfAccount= " + addPage.totalNumberOfAccount.size());
-
-
+        /* in case of no account on the main page, being able to see the automation of delete function
+           firstly we add new account for avoiding the situation of no account.
+        */
+        // click the add button
         addPage.addButton.click();
+        // implement thread sleep to prevent form web page and selenium driver inconsistencies
         BrowserUtils.sleep(3);
+        // enter info for record
         addPage.titleInputBox.sendKeys("This is the title");
         BrowserUtils.sleep(3);
+        // enter info for record
         addPage.idInputBox.sendKeys("");
+        // enter info for record
         addPage.phoneInputBox.sendKeys("+353 (0) 12939906");
         BrowserUtils.sleep(3);
+        // enter info for record
         addPage.emailInputBox.sendKeys("info@netaffinity.com");
         BrowserUtils.sleep(3);
+        // click save button
         addPage.saveButton.click();
+        // logs for log4j
         logger.info("new account was created.");
+        // logs for extent logger
         extentLogger.info("new account was created.");
         BrowserUtils.sleep(5);
+        // get total accounts number for comparing
         actualAccountNumber = addPage.totalNumberOfAccount.size();
+        // click delete button
         addPage.deleteButton.click();
         logger.info("the account was deleted.");
         extentLogger.info("the account was deleted.");
 
+        // used Explicit wait for catching pop up deletion message
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
         By popUpMessageElement = By.xpath("//span[@class='flex ng-binding']");
         WebElement toastElement = wait.until(ExpectedConditions.visibilityOfElementLocated(popUpMessageElement));
@@ -58,7 +72,7 @@ public class NetAffinityTest extends TestBase {
         String message = toastElement.getText();
         System.out.println("Pop-Up Message: " + message);
         BrowserUtils.sleep(5);
-
+        // After deletion, get the remaining of account number
         int expectedAccountNumber = addPage.totalNumberOfAccount.size();
         System.out.println("expectedAccountNumber = " + expectedAccountNumber);
 
